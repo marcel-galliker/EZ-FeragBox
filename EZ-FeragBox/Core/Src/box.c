c9d8362d1054f884d8bbc0fa6aadf77aa36996b9
Marcel Galliker <marcel@galliker-engineering.ch>
2024-08-07 16:08:05 +0200

    Implemented Flight Time Compensation

--- EZ-FeragBox/Core/Src/box.c
+++ EZ-FeragBox/Core/Src/box.c
@@ -248,6 +248,10 @@ static void _handle_feragMsg(void)
 					{
 						memcpy(&_Tracking[idx].prod, &_FeragMsg,  sizeof(_Tracking[idx].prod));
 						_Tracking[idx].delay = _PrintGoDelay;
+						if (_Status.enc.encOutSpeed<4000)
+						{
+							_Tracking[idx].delay += 50*(4000-_Status.enc.encOutSpeed)/4000; // max compensation 50 dots
+						}
 						/*// TEST overlap printing
 						if (idx==5)
 						{
@@ -262,10 +266,10 @@ static void _handle_feragMsg(void)
 					//	else printf("ProductDetect %d: PaceId=%d (EMPTY, encIn=%d, encOut=%d, delay=%d, pos=%d, dist=%d)\n", _Status.dtCnt, _FeragMsg.paceId, enc_get_pos(), _EncoderPos, _PrintGoDelay, _EncoderPos+_PrintGoDelay, dist);
 						if (_FeragMsg.info&1)
 						{
-							printf("DT%d: PaceId=%d, dist=%d, speedIn=%d\n", _Status.dtCnt, _FeragMsg.paceId, dist, _Status.enc.encInSpeed);
+							printf("DT%d: PaceId=%d, dist=%d, speedIn=%d out=%d\n", _Status.dtCnt, _FeragMsg.paceId, dist, _Status.enc.encInSpeed, _Status.enc.encOutSpeed);
 							_ProdDtCnt++;
 						}
-						else printf("DT%d: PaceId=%d (EMPTY) dist=%d)\n", _Status.dtCnt, _FeragMsg.paceId, dist);
+						else printf("DT%d: PaceId=%d (EMPTY) dist=%d, speedIn=%d out=%d)\n", _Status.dtCnt, _FeragMsg.paceId, dist,_Status.enc.encInSpeed, _Status.enc.encOutSpeed);
 						_Status.dtCnt++;
 					}
 					break;
